65952bcf32a32a7e5f6c146be5e627bc052f1e1f
Ruben Dias <rubenrgd@gmail.com>
2020-09-01 17:14:06 -0300

    Commit

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,37 +1,51 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // IMPORTANTE: SoC: Separation of Concerns (Separação de preocupações)
 // DTO : Data Transfer Object
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
     });
 
     delete user.password;
 
     return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: true });
-});
+  },
+);
 
 export default usersRouter;
